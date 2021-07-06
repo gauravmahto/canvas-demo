af3ee6418259889a1d359f9bf4f9fba00dd851ce
Gaurav <gauravm.git@gmail.com>
2021-07-06 16:45:26 +0530

    Initial commit

---
+++ modules/utils.js
@@ -0,0 +1,48 @@
+// export function addEventListener(element, handler) {
+
+//   element.addEventListener('mouseup', handler);
+
+// }
+
+export function nullOrUndefined(identifier) {
+
+  return (typeof identifier === 'undefined' ||
+    null === identifier);
+
+}
+
+export function getGlobal() {
+
+  let localGlobal;
+
+  if (nullOrUndefined(localGlobal) &&
+    typeof globalThis !== 'undefined') {
+
+    localGlobal = globalThis;
+
+  }
+
+  if (nullOrUndefined(localGlobal) &&
+    typeof global !== 'undefined') {
+
+    localGlobal = global;
+
+  }
+
+  if (nullOrUndefined(localGlobal) &&
+    typeof window !== 'undefined') {
+
+    localGlobal = window;
+
+  }
+
+  if (nullOrUndefined(localGlobal) &&
+    typeof self !== 'undefined') {
+
+    localGlobal = self;
+
+  }
+
+  return localGlobal;
+
+}
